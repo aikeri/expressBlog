e51bd442217818317c0ed245564ef39032df6246
aikeri <892344636@qq.com>
2018-09-11 17:45:13 +0800

    提交信息

--- modules/blog.js
+++ modules/blog.js
@@ -7,7 +7,9 @@ var blogSchema=new Schema({
 	authorid:{type:String},
 	viewcount : { type: Number},
 	createtime:{type:Date},
-	updatetime:{type:Date}
+	updatetime:{type:Date},
+	category:{type:String},
+	categoryid:{type:String}
 });
 
 module.exports=mongoose.model('Blog', blogSchema);
